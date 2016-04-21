9ecd1f8a287ec886a1cdc7d2966449ed94f536e0
miha <klimovm@ukr.net>
2016-04-21 18:13:18 +0300

    21.04

--- scr/kurs1/week2/Work2.java
+++ scr/kurs1/week2/Work2.java
@@ -1,5 +1,7 @@
 package scr.kurs1.week2;
 
+import java.util.Arrays;
+
 /**
  * Created by miha on 20.04.2016.
  */
@@ -7,4 +9,41 @@ package scr.kurs1.week2;
 //Поменять местами наибольший и наименьший элементы в массиве
 
 public class Work2 {
+    public static void main(String[] args) {
+        int[] mas = {2, 6, 8, 10, 3};
+
+        System.out.print("Входящий массив:  ");
+        for (int v : mas)
+            System.out.print(v + "  ");
+        System.out.println(" ");
+
+
+        int min = mas[0];
+        int max = mas[0];
+
+        for (int i = 0; i < mas.length; i++) {
+
+            if (min > mas[i])
+                min = mas[i];
+            if (max < mas[i])
+                max = mas[i];
+        }
+
+        System.out.println("Минимум " + min);
+        System.out.println("Максимум " + max);
+        System.out.print("исходящий массив:  ");
+
+        if (min != max)
+            for (int i = 0; i < mas.length; i++)
+                if (mas[i] == max)
+                    mas[i] = min;
+                else if (mas[i] == min)
+                    mas[i] = max;
+        for (int i = 0; i < mas.length; i++)
+
+            System.out.print(mas[i] + "  ");
+        System.out.println();
+
+    }
 }
+
--- scr/kurs1/week2/Work3.java
+++ scr/kurs1/week2/Work3.java
@@ -7,4 +7,37 @@ package scr.kurs1.week2;
 //Заданы два массива одинаковой длинны с любыми значениями скопировать данные из первого массива во второй
 
 public class Work3 {
+    public static void main(String[] args) {
+
+        int[] masiv1 = {9, 2, 6, 5, 7};
+        System.out.print("первый массив  ");
+        {
+            for (int v : masiv1)
+                System.out.print(v + " ");
+        }
+
+        System.out.println("   ");
+
+        int[] masiv2 = new int[5];
+        System.out.print("Второй массив  ");
+        {
+            for (int d : masiv2)
+                System.out.print(d + " ");
+        }
+
+        System.out.println(" ");
+        System.out.print("данные из первого массива во второй  ");
+        for (int i = 0; i < masiv1.length; i++) {
+            masiv2[i] = masiv1[i];
+        }
+//        masiv2[0]=masiv1[0];
+//        masiv2[1]=masiv1[1];
+//        masiv2[2]=masiv1[2];
+//        masiv2[3]=masiv1[3];
+//        masiv2[4]=masiv1[4];
+        for (int c : masiv2)
+            System.out.print(c + " ");
+
+
+    }
 }
