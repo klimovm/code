5fdfd0327f546d81a7c1df2c762d50a612267ff4
miha <klimovm@ukr.net>
2016-04-22 18:13:38 +0300

    21.04

---
+++ scr/kurs1/week2/Work3_Scanner.java
@@ -0,0 +1,50 @@
+package scr.kurs1.week2;
+
+import java.util.Scanner;
+
+/**
+ * Created by miha on 22.04.2016.
+ */
+public class Work3_Scanner {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Введите первый массив");
+        int[] masiv1 = new int[5];
+        for (int i = 0; i < masiv1.length; i++) {
+            System.out.println("Введите число " + (i + 1));
+            masiv1[i] = scn.nextInt();
+        }
+
+        System.out.println("Введите второй массив");
+        int[] masiv2 = new int[5];
+        for (int i = 0; i < masiv2.length; i++) {
+            System.out.println("Введите число " + (i + 1));
+            masiv2[i] = scn.nextInt();
+        }
+
+        System.out.print("первый массив  ");
+        {
+            for (int v : masiv1)
+                System.out.print(v + " ");
+            System.out.println(" ");
+        }
+
+        System.out.print("второй массив  ");
+        {
+            for (int v : masiv2)
+                System.out.print(v + " ");
+        }
+
+        System.out.println(" ");
+        System.out.print("данные из первого массива во второй  ");
+        for (int i = 0; i < masiv1.length; i++) {
+            masiv2[i] = masiv1[i];
+        }
+        for (int c : masiv2)
+            System.out.print(c + " ");
+
+
+    }
+}
--- scr/kurs1/week2/Work5.java
+++ scr/kurs1/week2/Work5.java
@@ -1,5 +1,7 @@
 package scr.kurs1.week2;
 
+import java.util.Scanner;
+
 /**
  * Created by miha on 20.04.2016.
  */
@@ -7,4 +9,13 @@ package scr.kurs1.week2;
 //Заполнить массив случайными значениями. На четных индексах парные значения, на нечетных непарные
 
 public class Work5 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            // generate number and put into arr
+            arr[i] = (int) (Math.random() * 100);
+        }
+
+    }
 }
