487990bd91ffff74d4ab45727b28daaca9be6a6f
miha <klimovm@ukr.net>
2016-04-25 10:09:05 +0300

    21.04

--- scr/kurs1/week2/Work6.java
+++ scr/kurs1/week2/Work6.java
@@ -7,4 +7,14 @@ package scr.kurs1.week2;
 //Найти среднее арифметическое массива
 
 public class Work6 {
+
+    public static void main(String[] args) {
+        int [] maviv = {4,4,4,4};
+        int srednee = 0;
+
+        for (int i = 0; i < maviv.length; i++){
+            srednee += maviv[i];
+        }
+        System.out.println(srednee / maviv.length);
+    }
 }
--- scr/kurs1/week2/Work7.java
+++ scr/kurs1/week2/Work7.java
@@ -1,9 +1,21 @@
 package scr.kurs1.week2;
 
+import java.util.Scanner;
+
 /**
  * Created by miha on 20.04.2016.
  */
 
 //Вывести в консоль элементы той половины одномерного массива у которой среднее арифметическое максимальное
 public class Work7 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int [] masiv = new int[4];
+        for (int i = 0; i < masiv.length; i++ ){
+            System.out.println("Введите "+ (i + 1) + "-й елемент массива");
+            masiv[i] = scn.nextInt();
+        }
+
+    }
 }
