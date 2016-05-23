7f6d31f10417f308e6db3bda102bcc325993718a
miha <klimovm@ukr.net>
2016-05-23 18:25:36 +0300

    23.05

--- scr/kurs2/week1/day2/Group.java
+++ scr/kurs2/week1/day2/Group.java
@@ -54,7 +54,7 @@ public class Group {
 
     public void sortName() {
         for (int i = students.length - 1; i > 0; i--) {
-            System.out.println(students[i]+" ");
+            System.out.println(students[i]);
         }
 
     }
