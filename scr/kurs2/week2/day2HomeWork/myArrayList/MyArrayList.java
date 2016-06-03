03be0f2c4195a848c31edfdc11e14e442dddf943
miha <klimovm@ukr.net>
2016-06-03 17:44:17 +0300

    03.06

---
+++ scr/kurs2/week2/day2HomeWork/myArrayList/MyArrayList.java
@@ -0,0 +1,16 @@
+package scr.kurs2.week2.day2HomeWork.myArrayList;
+
+
+
+/**
+ * Created by miha on 03.06.2016.
+ */
+public class MyArrayList {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_SIZE = 10;
+    private int counter;
+
+
+
+}
---
+++ scr/kurs2/week2/day2HomeWork/myArrayList/TestMyArrayList.java
@@ -0,0 +1,13 @@
+package scr.kurs2.week2.day2HomeWork.myArrayList;
+
+/**
+ * Created by miha on 03.06.2016.
+ */
+public class TestMyArrayList {
+    public static void main(String[] args) {
+        MyArrayList a = new MyArrayList();
+        Object  object1 = "Саша";
+
+
+    }
+}
