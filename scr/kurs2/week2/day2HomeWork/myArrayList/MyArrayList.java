44579bef5e6f87474f25c1e4088e62b2103df42b
miha <klimovm@ukr.net>
2016-06-06 17:31:20 +0300

    06.06

--- scr/kurs2/week2/day2HomeWork/myArrayList/MyArrayList.java
+++ scr/kurs2/week2/day2HomeWork/myArrayList/MyArrayList.java
@@ -1,29 +1,39 @@
 package scr.kurs2.week2.day2HomeWork.myArrayList;
 
 
-
 /**
  * Created by miha on 03.06.2016.
  */
 public class MyArrayList {
+
+
     private Object[] elements;
     private int size;
     private static final int DEFAULT_SIZE = 10;
+    private int counter;
 
 
-
-    public MyArrayList(Object[] elements) {
-        size = 0 ;
-        this.elements = elements;
+    public MyArrayList() {
+        elements = new Object[this.DEFAULT_SIZE];
     }
 
-    public boolean add (Object o){
-        if (o == null) return false;
-
+    /********************
+     * Добавление обїекта в ArrayList
+     ***************/
+    public boolean add(Object obj) {
+        if (obj == null) return false;
+        else if (counter < size) {
+            elements[counter++] = obj;
+        } else {
+            int sizeNew = size * 2;
+            Object[] elementsNew = new Object[sizeNew];
+            System.arraycopy(elements, 0, elementsNew, 0, size);
+            elements = elementsNew;
+            size = sizeNew;
+            counter++;
+        }
         return true;
-
     }
-
-
-
 }
+
+
--- scr/kurs2/week2/day2HomeWork/myArrayList/TestMyArrayList.java
+++ scr/kurs2/week2/day2HomeWork/myArrayList/TestMyArrayList.java
@@ -1,11 +1,23 @@
 package scr.kurs2.week2.day2HomeWork.myArrayList;
 
+import java.util.ArrayList;
+
 /**
  * Created by miha on 03.06.2016.
  */
 public class TestMyArrayList {
     public static void main(String[] args) {
-        MyArrayList a = new MyArrayList();
-        Object  object1 = "Саша";
+        Worker worker1 = new Worker("Саша","Петров",1988,1);
+        Worker worker2 = new Worker("Коля","Иванов",1989,2);
+        Worker worker3 = new Worker("Катя","Жукова",1986,4);
+
+        String a = "22";
+
+        MyArrayList list = new MyArrayList();
+
+        list.add(a);
+        System.out.println(list.toString());
+        }
+
+
     }
-}
