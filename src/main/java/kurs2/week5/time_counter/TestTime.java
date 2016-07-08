1def32b0ee7b532159403c9fc44044d5978292b0
miha <klimovm@ukr.net>
2016-07-08 09:39:50 +0300

    08.07

--- src/main/java/kurs2/week5/time_counter/TestTime.java
+++ src/main/java/kurs2/week5/time_counter/TestTime.java
@@ -3,45 +3,42 @@ package kurs2.week5.time_counter;
 import kurs2.utils.ArrayUtils;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by miha on 29.06.2016.
  */
 public class TestTime {
     public static void main(String[] args) throws IOException {
+
+
         TimeCounter bubleTime = new TimeCounter();
         final int[] array3 = ArrayUtils.createArray(10000);
-
+        Arrays.copyOf(array3, array3.length);
         long bubleSort = bubleTime.count(new Action() {
             @Override
             public void run() {
-                ArrayUtils.bubleSort(array3);
+                ArrayUtils.bubleSort(Arrays.copyOf(array3, array3.length));
             }
         });
         System.out.println("bubleSort - " + bubleSort);
 
 
-
         TimeCounter insertTime = new TimeCounter();
-        final int[] array2 = ArrayUtils.createArray(10000);
-
         long insertSort = insertTime.count(new Action() {
             @Override
             public void run() {
-                ArrayUtils.insertSort(array2);
+                ArrayUtils.insertSort(Arrays.copyOf(array3, array3.length));
             }
         });
         System.out.println("insertSort - " + insertSort);
 
 
-
         TimeCounter selectTime = new TimeCounter();
-        final int[] array1 = ArrayUtils.createArray(10000);
-
         long selectSort = selectTime.count(new Action() {
             @Override
             public void run() {
-                ArrayUtils.selectSort(array1);
+                ArrayUtils.selectSort(Arrays.copyOf(array3, array3.length));
             }
         });
         System.out.println("selectSort - " + selectSort);
